52a2883fd990302e7e46b5e85cbdc44f5b140de5
Ayemere <davidodia4@gmail.com>
2021-10-28 19:54:16 +0100

    Initial Commit

---
+++ Marmalade/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marmalade
+//
+//  Created by Ayemere  Odia  on 28/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Marmalade/MarmaladeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarmaladeApp.swift
+//  Marmalade
+//
+//  Created by Ayemere  Odia  on 28/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MarmaladeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
